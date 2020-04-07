d526cfb4a7f892c6d0d0e1375a76459c47a496e4
Nikita Rogalenko <nikitalr@mail.ru>
2020-04-07 14:33:15 +0300

    pa2: main from pa1

--- pa2/pa23.c
+++ pa2/pa23.c
@@ -52,6 +52,77 @@ int main(int argc, char const *argv[])
         sum_process_num = children_num + (uint8_t) 1;
     }
 
+    // logs init
+    log_init();
 
+    // creating read/write channels
+    io_channel_t *io_channel = create_pipes(sum_process_num);
+
+    // creating array of running processes
+    pid_t processes[sum_process_num];
+    // forking children
+    processes[0] = getpid();
+    int loopbreak = 0;
+    for (uint8_t pid = 1; pid <= children_num && !loopbreak; pid++)
+    {
+        int child_process = fork();
+        switch (child_process)
+        {
+            case -1:
+                // error
+                fprintf(stderr, "ERROR: fork failed\n");
+                return 1;
+            case 0:
+                // child process
+                io_channel->id = pid;
+                close_unused_pipes(io_channel);
+                loopbreak = 1;
+                break;
+            default:
+                // parent process
+                io_channel->id = PARENT_ID;
+                processes[pid] = child_process;
+                break;
+        }
+    }
+
+    close_unused_pipes(io_channel);
+
+    Message* started_message = create_message(MESSAGE_MAGIC, STARTED);
+    Message* done_message = create_message(MESSAGE_MAGIC, DONE);
+
+    if (io_channel->id == PARENT_ID)
+    {
+        // parent
+        receive_from_all_processes(io_channel); // receiving all STARTED
+        log_received_all_started(io_channel);
+        receive_from_all_processes(io_channel); // receiving all DONE
+        log_received_all_done(io_channel);
+        // waiting for processes to finish
+        for (uint8_t pid = 1; pid <= io_channel->children_num; pid++)
+            if (!waitpid(processes[pid], NULL, 0))
+            {
+                return 1;
+            }
+    }
+    else
+    {
+        // child
+        send_started(io_channel,started_message); // send to all - STARTED
+        log_started(io_channel);
+        receive_from_all_processes(io_channel); // receiving all STARTED
+        log_received_all_started(io_channel);
+        send_done(io_channel, done_message);   // send to all - DONE
+        log_done(io_channel);
+        receive_from_all_processes(io_channel); // receiving all DONE
+        log_received_all_done(io_channel);
+
+    }
+
+    log_events_close();
+    log_pipes_close();
+
+    return 0;
 
 }
+
